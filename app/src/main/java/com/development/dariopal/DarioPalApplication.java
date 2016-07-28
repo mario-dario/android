946090d7762f0ad0d56b327c5ee592e9746c8b95
artyom_okun <rt.okun@gmail.com>
2016-07-28 19:12:46 +0300

    Add first drawable

--- app/src/main/java/com/development/dariopal/DarioPalApplication.java
+++ app/src/main/java/com/development/dariopal/DarioPalApplication.java
@@ -1,10 +1,23 @@
 package com.development.dariopal;
 
 import android.app.Application;
+import android.content.Context;
 
 /**
  * Created by rt_okun on 28/07/2016.
  */
-public class DarioPalApplication  extends Application{
-    public static final int =
+public class DarioPalApplication extends Application {
+
+    private static Context mContext;
+
+    public static Context getContext() {
+        return mContext;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+    }
+
 }
