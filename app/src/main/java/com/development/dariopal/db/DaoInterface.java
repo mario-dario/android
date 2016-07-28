40ca8cb860def7eae9e0c7de35103f2f0b624a6a
Elad Keyshawn <elad.keyshawn@gmail.com>
2016-07-28 19:07:30 +0300

    Starting working on DarioPal db

--- app/src/main/java/com/development/dariopal/db/DaoInterface.java
+++ app/src/main/java/com/development/dariopal/db/DaoInterface.java
@@ -1,7 +1,18 @@
 package com.development.dariopal.db;
 
-/**
- * Created by Kishon on 7/28/2016.
- */
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public interface DaoInterface {
+
+  List<EventRecord> getAllRecords();
+
+  List<EventRecord> getRecordsInTimeRange(Time start, Time end);
+
+  List<EventRecord> getAllWithType (String type);
+
+  List<EventRecord> getAllWithTypeInRange(Time start, Time end, String type);
+
 }
--- app/src/main/java/com/development/dariopal/db/DarioPalDaoApi.java
+++ app/src/main/java/com/development/dariopal/db/DarioPalDaoApi.java
@@ -1,6 +1,37 @@
 package com.development.dariopal.db;
 
 
-public class DarioPalDaoApi {
+import java.sql.Time;
+import java.util.List;
+
+public class DarioPalDaoApi implements DaoInterface {
+
+  @Override
+  public List<EventRecord> getAllRecords() {
+    List<EventRecord> resultRecords = EventRecord.listAll(EventRecord.class);
+    return resultRecords;
+  }
+
+  @Override
+  public List<EventRecord> getRecordsInTimeRange(Time start, Time end) {
+      List<EventRecord> resultRecords = EventRecord.find(EventRecord.class,
+          "timeStamp <= ? and timeStamp >= ?", end.toString(), start.toString());
+    return resultRecords;
+  }
+
+  @Override
+  public List<EventRecord> getAllWithType(String type) {
+    List<EventRecord> resultRecords = EventRecord.find(EventRecord.class, "type = ?" , type);
+    return resultRecords;
+
+  }
+
+  @Override
+  public List<EventRecord> getAllWithTypeInRange(Time start, Time end, String type) {
+    List<EventRecord> resultRecords = EventRecord.find(EventRecord.class,
+        "timeStamp <= ? and timeStamp >= ? and type = ?", end.toString(), start.toString(), type);
+    return resultRecords;
+  }
+
 
 }
