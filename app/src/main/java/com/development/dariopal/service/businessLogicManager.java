5855e7f0ff217b8203d0170b6be318f268892104
mariotavor <mariotavor@gmail.com>
2016-07-29 12:20:20 +0300

    business logic update

--- app/src/main/java/com/development/dariopal/service/businessLogicManager.java
+++ app/src/main/java/com/development/dariopal/service/businessLogicManager.java
@@ -3,9 +3,13 @@ package com.development.dariopal.service;
 import android.content.Context;
 
 import com.development.dariopal.database.DBManagerInterface;
+import com.development.dariopal.database.EventRecord;
 import com.development.dariopal.neura_manager.NeuraEventConstants;
 import com.development.dariopal.notifications.NotificationWrapper;
 import com.neura.standalonesdk.events.NeuraEvent;
+
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by mario on 29/07/16.
@@ -15,9 +19,12 @@ public class businessLogicManager
     private final Context mDarioPalService;
     private final DBManagerInterface mDbManagerInterface;
     private final NotificationWrapper mNotificationManager;
+    public final  String TAG;
 
     public businessLogicManager(Context darioPalService, DBManagerInterface dbManagerInterface, NotificationWrapper notificationManager)
     {
+       TAG = this.getClass().getName();
+
         mDarioPalService=darioPalService;
         mDbManagerInterface=dbManagerInterface;
         mNotificationManager=notificationManager;
@@ -29,8 +36,8 @@ public class businessLogicManager
     {
         if (event.getEventName().equals(NeuraEventConstants.USER_ARRIVED_TO_GYM))
         {
-            if(isLastMeasurementAbnormal())
-                mNotificationManager.updateNotification("test","test body");
+            isLastMeasurementAbnormal(event);
+            mNotificationManager.updateNotification("test","test body");
         }
 
 //        }else if(event.getEventName().equals(NeuraEventConstants.USER_LEFT_THE_GYM)
@@ -40,8 +47,29 @@ public class businessLogicManager
 //        }
     }
 
-    private boolean isLastMeasurementAbnormal()
+    private boolean isLastMeasurementAbnormal(NeuraEvent event)
     {
-        return true;
+        Calendar inHourBack = Calendar.getInstance();
+        inHourBack.setTimeInMillis(event.getEventTimestamp());
+        inHourBack.add(Calendar.HOUR, -1);
+
+        List<EventRecord> eventRecordList = mDbManagerInterface.onGetFromDB(inHourBack.getTimeInMillis(),event.getEventTimestamp());
+        if(eventRecordList.size()>0)
+            return true;
+//
+//        Log.e(TAG,"getTimeInMillis start" + inHourBack.getTimeInMillis());
+//        Log.e(TAG,"getTimeInMillis end" + event.getEventTimestamp());
+
+
+
+//        //List<EventRecord> eventAllRecordList = mDbManagerInterface.getAllRecordFromDB();
+//        for (EventRecord REvent:eventRecordList)
+//        {
+////            Log.e(TAG,"REvent.getType()" + REvent.getType());
+////            Log.e(TAG,"REvent.getValue()" + REvent.getValue());
+////            Log.e(TAG,"REvent.getTimeStamp()" + REvent.getTimeStamp());
+//        }
+//        return true;
+        return false;
     }
 }
