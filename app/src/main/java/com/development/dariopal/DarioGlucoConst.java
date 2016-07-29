288e0d51bc46b8ec35e9239bd227991c9ea13582
mariotavor <mariotavor@gmail.com>
2016-07-29 09:16:00 +0300

    new DarioGlucoseConst class

---
+++ app/src/main/java/com/development/dariopal/DarioGlucoConst.java
@@ -0,0 +1,10 @@
+package com.development.dariopal;
+
+/**
+ * Created by mario on 29/07/16.
+ */
+
+public abstract class DarioGlucoConst
+{
+    public static final String DARIO_LAUNCHER_ACTIVITY = "com.labstyle.darioandroid.welcome.WelcomeActivity";
+}
